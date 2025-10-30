25295dc14aa943678e2eb6d4a1f6296d8a0e8256
jmizerka <j.mizerka98@gmail.com>
2025-10-30 21:04:58 +0100

    routers registration in app

--- app/main.py
+++ app/main.py
@@ -1,4 +1,18 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from app.db.session import init_db
+from app.routers import books_router, borrowers_router, loans_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(borrowers_router)
+app.include_router(books_router)
+app.include_router(loans_router)
