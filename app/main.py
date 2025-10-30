e049997e5b9fb2d9c9106d90a81fb322eac15179
jmizerka <j.mizerka98@gmail.com>
2025-10-30 21:36:45 +0100

    cors middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.db.session import init_db
 from app.routers import books_router, borrowers_router, loans_router
@@ -13,6 +14,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # in production origins would be restricted
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(borrowers_router)
 app.include_router(books_router)
 app.include_router(loans_router)
