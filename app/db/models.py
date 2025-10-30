edd5b5420639fb6cb3e61e3480f00bee8795e91e
jmizerka <j.mizerka98@gmail.com>
2025-10-30 20:19:44 +0100

    book table

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,14 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import CheckConstraint, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class Book(Base):
+    __tablename__ = "books"
+    id: Mapped[str] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    serial_num: Mapped[str] = mapped_column(String(6), unique=True, index=True)  # indexing for future filtering option
+    title: Mapped[str] = mapped_column(String, nullable=False, index=True)
+    author: Mapped[str] = mapped_column(String, nullable=False, index=True)
