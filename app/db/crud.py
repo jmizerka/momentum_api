9f79f219b97c6c4c9aec03d620fe12ac5de084c8
jmizerka <j.mizerka98@gmail.com>
2025-10-30 20:45:37 +0100

    crud for loan

--- app/db/crud.py
+++ app/db/crud.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
@@ -115,5 +117,72 @@ async def delete_borrower(session: AsyncSession, borrower_id: id) -> bool:
     if not borrower:
         return False
     await session.delete(borrower)
+    await session.commit()
+    return True
+
+
+async def create_loan(session: AsyncSession, book_id: int, borrower_id: int, borrow_date: datetime = None) -> Loan:
+    borrow_date = borrow_date or datetime.now()
+    book = await get_book(session, book_id)
+    borrower = await get_borrower(session, borrower_id)
+    if not book or not borrower:
+        raise ValueError("Book or Borrower not found")
+
+    loan = Loan(borrow_date=borrow_date, book_id=book.id, borrower_id=borrower.id)
+    loan.book = book
+    loan.borrower = borrower
+    session.add(loan)
+    await session.commit()
+    await session.refresh(loan)
+    return loan
+
+
+async def get_loan(session: AsyncSession, loan_id: int) -> Loan | None:
+    return await session.get(Loan, loan_id)
+
+
+async def get_all_loans(
+    session: AsyncSession,
+    skip: int | None = None,
+    limit: int | None = None,
+    borrower_card_number: str | None = None,
+    book_serial_num: str | None = None,
+    returned: bool | None = None,
+) -> list[Loan]:
+    query = select(Loan).options(selectinload(Loan.borrower), selectinload(Loan.book))
+
+    if borrower_card_number:
+        query = query.join(Loan.borrower).where(Borrower.card_number == borrower_card_number)
+    if book_serial_num:
+        query = query.join(Loan.book).where(Book.serial_num == book_serial_num)
+    if returned is not None:
+        if returned:
+            query = query.where(Loan.return_date.is_not(None))
+        else:
+            query = query.where(Loan.return_date.is_(None))
+
+    if skip is not None:
+        query = query.offset(skip)
+    if limit is not None:
+        query = query.limit(limit)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+async def update_loan_return_date(session: AsyncSession, loan_id: int, return_date: datetime) -> Loan | None:
+    loan = await get_loan(session, loan_id)
+    if not loan:
+        return None
+    loan.return_date = return_date
+    await session.commit()
+    await session.refresh(loan)
+    return loan
+
+
+async def delete_loan(session: AsyncSession, loan_id: int) -> bool:
+    loan = await get_loan(session, loan_id)
+    if not loan:
+        return False
+    await session.delete(loan)
     await session.commit()
     return True
