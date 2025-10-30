4a22bd4dafafb271b10ffcb1f8fd6298ffaedd8d
jmizerka <j.mizerka98@gmail.com>
2025-10-30 20:44:46 +0100

    crud for borrower

--- app/db/crud.py
+++ app/db/crud.py
@@ -2,7 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
-from app.db.models import Book, Loan
+from app.db.models import Book, Loan, Borrower
 
 
 async def create_book(session: AsyncSession, serial_num: str, title: str, author: str) -> Book:
@@ -65,5 +65,55 @@ async def delete_book(session: AsyncSession, book_id: int) -> bool:
     if not book:
         return False
     await session.delete(book)
+    await session.commit()
+    return True
+
+
+async def create_borrower(session: AsyncSession, card_number: str) -> Borrower:
+    borrower = Borrower(card_number=card_number)
+    session.add(borrower)
+    await session.commit()
+    await session.refresh(borrower)
+    return borrower
+
+
+async def get_borrower(session: AsyncSession, borrower_id: int) -> Borrower | None:
+    result = await session.execute(select(Borrower).where(Borrower.id == borrower_id))
+    return result.scalar_one_or_none()
+
+
+async def get_all_borrowers(
+    session: AsyncSession,
+    skip: int | None = None,
+    limit: int | None = None,
+    card_number: str | None = None,
+) -> list[Borrower]:
+    query = select(Borrower).options(selectinload(Borrower.loans).selectinload(Loan.book))
+    if card_number:
+        query = query.where(Borrower.card_number == card_number)
+    if skip is not None:
+        query = query.offset(skip)
+    if limit is not None:
+        query = query.limit(limit)
+
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+async def update_borrower(session: AsyncSession, borrower_id: int, new_card_number: str) -> Borrower | None:
+    borrower = await get_borrower(session, borrower_id)
+    if not borrower:
+        return None
+    borrower.card_number = new_card_number
+    await session.commit()
+    await session.refresh(borrower)
+    return borrower
+
+
+async def delete_borrower(session: AsyncSession, borrower_id: id) -> bool:
+    borrower = await get_borrower(session, borrower_id)
+    if not borrower:
+        return False
+    await session.delete(borrower)
     await session.commit()
     return True
